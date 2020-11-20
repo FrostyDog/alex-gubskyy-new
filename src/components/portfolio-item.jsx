4933e9b958d4cb3d2d68dedd677f7a00d06252d3
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-11-20 23:07:49 +0100

    feat: add contact

--- src/components/portfolio-item.jsx
+++ src/components/portfolio-item.jsx
@@ -1,1 +1,16 @@
-import React from "react"
+import React from "react";
+import "../css/components/portfolio-item.css";
+
+
+export default function PortfolioItem(props) {
+    const classPosition = "portfolio-item " + props.classPositon
+return (
+    <div className={classPosition}>
+        <a target="_blank" rel="noopener noreferrer" href={props.source}>
+        <div className="background" style={{backgroundImage: `url(${props.background})`}}></div>
+        <div className="background-overlay"></div>
+        </a>
+
+    </div>
+)
+}
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import HideSvg from "./hidesvg";
-
+import PortfolioItem from "./portfolio-item";
 
 export default function Portfolio(props) {
-
   let StatusClass = props.trackStatus(props.status);
 
   return (
@@ -11,6 +10,11 @@ export default function Portfolio(props) {
       <div onClick={() => props.changeStatus("portfolio")}>
         <HideSvg />
       </div>
+      <PortfolioItem source="http://alexgubskyy.com" background="https://images.unsplash.com/photo-1601277237358-93d22efff93f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80" classPositon="position-1"/>
+      <PortfolioItem classPositon="position-2"/>
+      <PortfolioItem classPositon="position-3"/>
+      <PortfolioItem classPositon="position-4"/>
+
     </section>
   );
 }

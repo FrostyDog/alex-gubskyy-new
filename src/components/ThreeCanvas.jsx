5793d6f2aa9daf304f89ccc8276b351c41c23535
Alex Gubskyy <aleks.gubskyy@gmail.com>
2021-03-11 01:57:55 +0100

    feat: adding objects

--- src/components/ThreeCanvas.jsx
+++ src/components/ThreeCanvas.jsx
@@ -10,33 +10,74 @@ function Box(props) {
   const [active, setActive] = useState(false);
 
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    mesh.current.position.y = mesh.current.position.y + (Math.sin(time * 2) / 200);
+    mesh.current.rotation.y = mesh.current.rotation.x += 0.01;
   });
 
   return (
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      // scale={active ? [1.5, 1.5, 1.5] : [0.5, 0.5, 0.5]}
+      scale={active ? [0.3, 0.4, 0.2] : [0.15, 0.15, 0.15]}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
       <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      <meshStandardMaterial color={hovered ? "orange" : "blue"} />
     </mesh>
   );
 }
 
+//  Utility
+function rightDiag(startPosition) {
+  let sum = [];
+  let [x, y, z] = startPosition;
+
+  for (let i = 0; i < 10; i++) {
+    x += 0.2;
+    y += 0.5;
+    sum.push([x, y, z]);
+  }
+  return sum;
+}
+
+function leftDiag(startPosition) {
+  let sum = [];
+  let [x, y, z] = startPosition;
+
+  for (let i = 0; i < 10; i++) {
+    x += 0.2;
+    y -= 0.5;
+    sum.push([x, y, z]);
+  }
+  return sum;
+}
+
+//
+
 export default function ThreeCanvas(props) {
   return (
-    <div className="canvasWrapper" >
+    <div className="canvasWrapper">
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        {rightDiag([-2.5, -3, 0]).map((el) => (
+          <Box position={el} />
+        ))}
+        {rightDiag([-2.6, -2.8, 0]).map((el) => (
+          <Box position={el} />
+        ))}
+
+        {leftDiag([-0.55, 2.7, 0]).map((el) => (
+          <Box position={el} />
+        ))}
+        {leftDiag([-0.65, 2.5, 0]).map((el) => (
+          <Box position={el} />
+        ))}
       </Canvas>
     </div>
   );
